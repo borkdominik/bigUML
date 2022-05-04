a724ab3aaf2a9c3e3b83cd8b33519ceafb78994a
Julian Weiß <julian.weiss94@live.de>
2022-05-04 11:59:54 +0200

    reworked context provider and added possibility to create edge between selected nodes via context menu

--- server/com.eclipsesource.uml.glsp/src/main/java/com/eclipsesource/uml/glsp/contextmenu/UmlContextMenuItemProvider.java
+++ server/com.eclipsesource.uml.glsp/src/main/java/com/eclipsesource/uml/glsp/contextmenu/UmlContextMenuItemProvider.java
@@ -9,6 +9,7 @@ import org.eclipse.glsp.graph.GPoint;
 import org.eclipse.glsp.server.features.contextmenu.ContextMenuItemProvider;
 import org.eclipse.glsp.server.features.contextmenu.MenuItem;
 import org.eclipse.glsp.server.model.GModelState;
+import org.eclipse.glsp.server.operations.CreateEdgeOperation;
 import org.eclipse.glsp.server.operations.CreateNodeOperation;
 
 import java.util.Arrays;
@@ -31,19 +32,48 @@ public class UmlContextMenuItemProvider implements ContextMenuItemProvider {
       UmlModelState umlModelState = UmlModelState.getModelState(modelState);
       Representation diagramType = UmlModelState.getModelState(umlModelState).getNotationModel().getDiagramType();
 
+      List<MenuItem> contextMenu = Lists.newArrayList();
+      switch (diagramType) {
+         case CLASS:
+            contextMenu.addAll(classDiagramNodes(position));
+            if (selectedElementIds.size() == 2) {
+               contextMenu.addAll(classDiagramEdges(selectedElementIds.get(0), selectedElementIds.get(1)));
+            }
+            return contextMenu;
+         //TODO
+         case ACTIVITY:
+         case OBJECT:
+         case USECASE:
+         case DEPLOYMENT:
+         case STATEMACHINE:
+            return contextMenu;
 
-      if (diagramType == Representation.CLASS) {
-         MenuItem createClass = new MenuItem(Types.CLASS, "Class",
-               Arrays.asList(new CreateNodeOperation(Types.CLASS, position)), true);
-         MenuItem createEnumeration = new MenuItem(Types.ENUMERATION, "Enumeration",
-               Arrays.asList(new CreateNodeOperation(Types.ENUMERATION, position)), true);
-         MenuItem createInterface = new MenuItem(Types.INTERFACE, "Interface",
-               Arrays.asList(new CreateNodeOperation(Types.INTERFACE, position)), true);
-
-         return Lists.newArrayList(createClass, createInterface, createEnumeration);
       }
+      return contextMenu;
+   }
 
-      // TODO: other diagram types
-      return Lists.newArrayList();
+   public List<MenuItem> classDiagramNodes(final GPoint position) {
+      MenuItem createClass = new MenuItem(Types.CLASS, "Class",
+            Arrays.asList(new CreateNodeOperation(Types.CLASS, position)), true);
+      MenuItem createEnumeration = new MenuItem(Types.ENUMERATION, "Enumeration",
+            Arrays.asList(new CreateNodeOperation(Types.ENUMERATION, position)), true);
+      MenuItem createInterface = new MenuItem(Types.INTERFACE, "Interface",
+            Arrays.asList(new CreateNodeOperation(Types.INTERFACE, position)), true);
+
+      MenuItem classDiagramNodes = new MenuItem("classDiagramNodes", "Nodes",
+            Arrays.asList(createClass, createEnumeration, createInterface), "classDiagramNodes");
+      return Lists.newArrayList(classDiagramNodes);
+   }
+
+   public List<MenuItem> classDiagramEdges(final String sourceElementId, final String targetElementId) {
+      MenuItem createAssociation = new MenuItem(Types.ASSOCIATION, "Association",
+            Arrays.asList(new CreateEdgeOperation(Types.ASSOCIATION, sourceElementId, targetElementId)), true);
+      MenuItem createGeneralization = new MenuItem(Types.CLASS_GENERALIZATION, "Generalization",
+            Arrays.asList(new CreateEdgeOperation(Types.CLASS_GENERALIZATION, sourceElementId, targetElementId)), true);
+
+      MenuItem classDiagramEdges = new MenuItem("classDiagramEdges", "Edges",
+            Arrays.asList(createAssociation, createGeneralization), "classDiagramEdges");
+
+      return Lists.newArrayList(classDiagramEdges);
    }
 }

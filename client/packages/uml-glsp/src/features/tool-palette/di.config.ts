6a356b55d51aaa6ecf640406c7d26dbed0f42cb2
Haydar Metin <hayd.metin@gmail.com>
2023-03-08 23:39:25 +0100

    Add explicit dependencies

--- client/packages/uml-glsp/src/features/tool-palette/di.config.ts
+++ client/packages/uml-glsp/src/features/tool-palette/di.config.ts
@@ -13,8 +13,9 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import '@borkdominik-bigUML/uml-glsp/css/extensions/tool-palette.css';
 import '@eclipse-glsp/client/css/tool-palette.css';
+
+import '../../../css/extensions/tool-palette.css';
 
 import { configureActionHandler, EnableDefaultToolsAction, EnableToolPaletteAction, TYPES } from '@eclipse-glsp/client';
 import { ContainerModule } from 'inversify';

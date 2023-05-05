8f5574ecf3d32fc90ca38513bd266dc9a4bb2d74
Haydar Metin <hayd.metin@gmail.com>
2023-05-05 20:07:53 +0200

    Fix license in client

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -31,5 +31,22 @@ module.exports = {
         ],
         '@typescript-eslint/no-shadow': ['error'],
         '@typescript-eslint/no-var-requires': 'off',
+        'header/header': [
+            2,
+            'block',
+            [
+                {
+                    pattern: '[\n\r]+ \\* Copyright \\([cC]\\) \\d{4}(-\\d{4})? .*[\n\r]+',
+                    template: `********************************************************************************
+* Copyright (c) 2023 borkdominik and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the MIT License which is available at https://opensource.org/licenses/MIT.
+*
+* SPDX-License-Identifier: MIT
+********************************************************************************`
+                }
+            ]
+        ]
     }
 };
--- client/packages/uml-theia-integration/src/browser/theia/new-file/new-file.menu-contribution.ts
+++ client/packages/uml-theia-integration/src/browser/theia/new-file/new-file.menu-contribution.ts
@@ -1,13 +1,11 @@
-/********************************************************************************
- * Copyright (c) 2021 EclipseSource and others.
+/*********************************************************************************
+ * Copyright (c) 2023 borkdominik and others.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0, or the MIT License which is
- * available at https://opensource.org/licenses/MIT.
+ * terms of the MIT License which is available at https://opensource.org/licenses/MIT.
  *
- * SPDX-License-Identifier: EPL-2.0 OR MIT
- ********************************************************************************/
+ * SPDX-License-Identifier: MIT
+ *********************************************************************************/
 import { MenuContribution, MenuModelRegistry } from '@theia/core';
 import { CommonMenus } from '@theia/core/lib/browser';
 import { NavigatorContextMenu } from '@theia/navigator/lib/browser/navigator-contribution';

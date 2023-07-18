29d5e8b87c1fe2547eadae2702e7214054f1f7cf
Haydar Metin <hayd.metin@gmail.com>
2023-07-18 19:46:06 +0200

    Fix position of created child node

--- server/com.eclipsesource.uml.glsp/src/main/java/com/eclipsesource/uml/glsp/uml/handler/operations/create/CreateLocationAwareNodeHandler.java
+++ server/com.eclipsesource.uml.glsp/src/main/java/com/eclipsesource/uml/glsp/uml/handler/operations/create/CreateLocationAwareNodeHandler.java
@@ -12,16 +12,26 @@ package com.eclipsesource.uml.glsp.uml.handler.operations.create;
 
 import java.util.Optional;
 
+import org.eclipse.glsp.graph.GCompartment;
+import org.eclipse.glsp.graph.GModelElement;
 import org.eclipse.glsp.graph.GPoint;
 import org.eclipse.glsp.server.operations.CreateNodeOperation;
 import org.eclipse.glsp.server.utils.LayoutUtil;
 
 import com.eclipsesource.uml.glsp.core.model.UmlModelState;
+import com.eclipsesource.uml.glsp.uml.gmodel.builder.CompartmentGBuilder;
 
 public interface CreateLocationAwareNodeHandler {
    default Optional<GPoint> relativeLocationOf(final UmlModelState modelState, final CreateNodeOperation operation) {
       var absoluteLocation = operation.getLocation();
       var container = modelState.getIndex().get(operation.getContainerId()).orElseGet(modelState::getRoot);
-      return absoluteLocation.map(location -> LayoutUtil.getRelativeLocation(location, container));
+
+      return absoluteLocation.map(location -> LayoutUtil.getRelativeLocation(location,
+         getChildrenContainer(modelState, container).orElse(container)));
+   }
+
+   default Optional<GModelElement> getChildrenContainer(final UmlModelState modelState, final GModelElement container) {
+      return container.getChildren().stream().filter(GCompartment.class::isInstance)
+         .filter(comp -> comp.getArgs().containsKey(CompartmentGBuilder.childrenContainerKey)).findFirst();
    }
 }

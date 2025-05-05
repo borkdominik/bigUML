09564cb772dd3c64def80bb842bc0f726356fe0d
mpetojev <marsapetojevic@gmmail.com>
2025-05-05 17:10:43 +0200

    rename component to timeline and delete increase buttons
    (cherry picked from commit 32507ce52e976b28503abdfaa7e6fdfaec76f979)

--- client/packages/big-revision-management/src/browser/revision-management.component.tsx
+++ client/packages/big-revision-management/src/browser/revision-management.component.tsx
@@ -6,36 +6,15 @@
  *
  * SPDX-License-Identifier: MIT
  **********************************************************************************/
-import { VSCodeContext } from '@borkdominik-biguml/big-components';
+
 import { useCallback, useContext, useEffect, useState, type ReactElement } from 'react';
-import { RevisionManagementResponse, RequestRevisionManagementAction } from '../common/index.js';
 
 
 export function RevisionManagement(): ReactElement {
-    const { listenAction, dispatchAction } = useContext(VSCodeContext);
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-        listenAction(action => {
-            if (RevisionManagementResponse.is(action)) {
-                setCount(action.count);
-            }
-        });
-    }, [listenAction]);
-
-    const increase1 = useCallback(() => {
-        dispatchAction(RequestRevisionManagementAction.create({ increase: 1 }));
-    }, [dispatchAction]);
-
-    const increase5 = useCallback(() => {
-        dispatchAction(RequestRevisionManagementAction.create({ increase: 5 }));
-    }, [dispatchAction]);
-
+ 
     return (
         <div>
-            <span>Revision Management {count}</span>
-            <button onClick={() => increase1()}>Increase 1</button>
-            <button onClick={() => increase5()}>Increase 5</button>
+            
         </div>
     );
 }

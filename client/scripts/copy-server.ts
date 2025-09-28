748a2593bed32b5c1c8430869d21512a3a098c95
Haydar Metin <dev@haydar.metin.at>
2025-09-28 13:02:39 +0200

    Use label names for search and fix property multiplicity direct edit (#184)

--- client/scripts/copy-server.ts
+++ client/scripts/copy-server.ts
@@ -6,13 +6,17 @@
  *
  * SPDX-License-Identifier: MIT
  *********************************************************************************/
-import { join } from 'path';
-import { copyBackendFile, log } from './copy-utils';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { copyBackendFile, log } from './copy-utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Idea taken from https://github.com/eclipse-emfcloud/coffee-editor/blob/master/client/coffee-servers/scripts/copy-servers.ts
 
 const BACKEND_VERSION = '0.1.0';
-const targetDirs = [join(__dirname, '..', 'packages', 'uml-vscode-integration', 'extension', 'server')];
+const targetDirs = [join(__dirname, '..', 'application', 'vscode', 'server')];
 
 // Backend Java CLI
 const path = join(__dirname, '..', '..', 'server', 'app', 'build', 'libs');

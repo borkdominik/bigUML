186472a9f607ea5eeffbdf2961f3098866b5ac12
Fisnik Miftari <e11776840@student.tuwien.ac.at>
2026-02-04 14:19:17 +0100

    move functionality to handlebars.ts

--- client/packages/big-code-generation/src/vscode/index.ts
+++ client/packages/big-code-generation/src/vscode/index.ts
@@ -7,58 +7,9 @@
  * SPDX-License-Identifier: MIT
  **********************************************************************************/
 
+import { registerHandlebarsHelpers } from './handlebars.js';
+
 export * from './code-generation.module.js';
 export * from './code-generation.provider.js';
 
-import { getUMLObjectType, type UMLOwnedParameter } from '@borkdominik-biguml/uml-protocol';
-import Handlebars from 'handlebars';
-
-Handlebars.registerHelper('isdefined', function (value) {
-    return value !== undefined;
-});
-
-Handlebars.registerHelper('isClass', function (value) {
-    if (!value || typeof value.eClass !== 'string') {
-        throw new TypeError('Expected `this.eClass` to be a string');
-    }
-    return getUMLObjectType(value) === 'Class';
-});
-
-Handlebars.registerHelper('isInterface', function (value) {
-    if (!value || typeof value.eClass !== 'string') {
-        throw new TypeError('Expected `this.eClass` to be a string');
-    }
-    return getUMLObjectType(value) === 'Interface';
-});
-
-Handlebars.registerHelper('isEnumeration', function (value) {
-    if (!value || typeof value.eClass !== 'string') {
-        throw new TypeError('Expected `this.eClass` to be a string');
-    }
-    return getUMLObjectType(value) === 'Enumeration';
-});
-
-Handlebars.registerHelper('isPackagePrivateModifier', function (value) {
-    if (!value) {
-        return true;
-    }
-    if (typeof value !== 'string') {
-        throw new TypeError('Expected `this` to be a string');
-    }
-    return value === 'package';
-});
-
-Handlebars.registerHelper('returnType', function (parameters: UMLOwnedParameter[]) {
-    for (const parameter of parameters) {
-        if (parameter.direction === 'return') {
-            const tmp = parameter.type as any;
-            return tmp.name;
-        }
-    }
-
-    return 'void';
-});
-
-Handlebars.registerHelper('parameters', function (parameters: UMLOwnedParameter[]) {
-    return parameters.filter(parameter => parameter.direction !== 'return');
-});
+registerHandlebarsHelpers();
